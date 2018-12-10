7af75f46401fc125f927303b28387fe50039dc14
黄江波 <jiangbo.huang@changhong.com>
2018-12-10 10:07:06 +0800

    DOCKERFILE

---
+++ Docker/buildimages.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+
+imageName="reg.changhong.io/nbiot/nbiot-api"
+Version=$1
+
+if [ -z "$Version" ] ;then
+  Version=1.0
+fi
+
+
+function errOut {
+  if [ $? -ne 0 ];then
+    echo "$1"
+    exit 1
+  fi
+}
+
+function echoPre {
+  echo "=============================="
+}
+
+
+function checkDocker {
+  command -v docker >/dev/null 2>&1
+  existsDocker=$?
+  if [ $existsDocker -ne 0 ];then
+     echoPre
+    echo "未安装docker服务"
+    yum install docker -y >/dev/null 2>&1 && systemctl start docker || errOut "安装docker出错！"
+    echo "安装docker成功!"
+     echoPre
+  fi
+}
+
+function build {
+  echoPre
+  echo "build nbiot-api"
+  sh ./build.sh || errOut "构建nbiot-api应用程序出错！"
+  echo "build nbiot-api succeed"
+  echoPre
+}
+
+function buildImage  {
+  echoPre
+  docker build -t ${imageName}:${Version} -f ../docker/Dockerfile ../ || errOut "构建${imageName}:${Version}镜像错误！"
+  echo "构建${imageName}:${Version}镜像成功！"
+  echoPre
+}
+
+
+function pushImage {
+  echoPre
+  docker push ${imageName}:${Version}
+  echoPre
+}
+
+function clearImage {
+  echoPre
+  docker rmi --force `docker images | grep "<none>"| awk '{print $3}'` &>/dev/null  && echo "删除无用镜像成功"
+  echoPre
+}
+
+function main {
+  checkDocker
+  build
+  buildImage
+  pushImage
+  clearImage
+}
+
+main
+exit 0
---
+++ Docker/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [  -z "${JAVA_OPTS}" ]
+then
+	JAVA_OPTS="-server -Xmx512M -Xms512M -Xmn375M -XX:+PrintFlagsFinal -XX:+PrintGCDetails -Dfile.encoding=UTF8 -Duser.timezone=GMT+08"
+fi
+if [ -z "${ENV}" ]
+then
+    ENV="dev"
+fi
+java ${JAVA_OPTS} -Denv=$ENV -Dfile.encoding=UTF-8 -jar /app/service-manage.jar
